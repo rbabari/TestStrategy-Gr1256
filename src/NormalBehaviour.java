8d9df8277b0437babcdb7b96d03cb0c3e4fe6c02
rbabari <r.babari@gmail.com>
2024-01-25 16:27:02 -0500

    Initial commit

---
+++ src/NormalBehaviour.java
@@ -0,0 +1,7 @@
+public class AgressifBehaviour implements IBehaviour {
+    @Override
+    public int moveCommand() {
+        System.out.println("Agressif Behaviour ");
+        return 1;
+    }
+}
